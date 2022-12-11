84c336fadc31eb56708582e74f285a4c12b21e35
yuinchirn <l.serah.yuinch@gmail.com>
2022-12-11 23:47:08 +0900

    fix log

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const SLEEP_MSEC = 180 * 1000;
 const app = express();
 
 app.get('/healthcheck', (req, res) => {
-  res.send('healthcheck:OK');
+  res.send('healthcheck: OK');
 });
 
 app.get('/sleep', (req, res) => {
-  setTimeout(() => res.send('sleep:OK'), SLEEP_MSEC);
+  setTimeout(() => res.send('sleep: OK'), SLEEP_MSEC);
 });
 
 const server = app.listen(80, () => console.log('Example app listening on port 80!'));
